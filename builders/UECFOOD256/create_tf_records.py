a5a78d1d8f8292532405c4e04175e3131d9d94a7
Pierre Ecarlat <pierre.ecarlat@finc.com>
2017-08-02 10:01:53 +0900

    deal with outbounding boxes

--- builders/UECFOOD256/create_tf_records.py
+++ builders/UECFOOD256/create_tf_records.py
@@ -99,10 +99,10 @@ def dict_to_tf_example(data,
   classes = []
   classes_text = []
   for ix, obj in enumerate(data['boxes']):
-    xmin.append(float(obj[0]) / width)
-    ymin.append(float(obj[1]) / height)
-    xmax.append(float(obj[2]) / width)
-    ymax.append(float(obj[3]) / height)
+    xmin.append(np.clip(float(obj[0]) / width), 0, 1)
+    ymin.append(np.clip(float(obj[1]) / height), 0, 1)
+    xmax.append(np.clip(float(obj[2]) / width), 0, 1)
+    ymax.append(np.clip(float(obj[3]) / height), 0, 1)
     classes_text.append(categories[int(data['labels'][ix])-1])
     classes.append(int(data['labels'][ix]))
 
